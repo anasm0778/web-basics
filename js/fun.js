46407de674e5769bda9fa5e34e81016e8dbe3907
anas <anasm0778@gmail.com>
2023-08-27 20:09:32 +0530

    commit

---
+++ js/fun.js
@@ -0,0 +1,24 @@
+// create a funtion using classic syntax
+function showAlert(){
+    alert("I am from classic funtion");
+}
+
+// funtion stored in a variable 
+f = function(){
+    console.log("I am from  funtiuon stored in avariable");
+}
+
+// es6 arrow funtion
+g = () => {
+    console.log("I am from arrow funtion");
+}
+
+// classic func with parameters 
+function add(a,b){
+   return a+b;
+}
+
+// arrow functionn  with parameters 
+add2 = (a,b) => {
+    console.log(a+b);
+}
